cfdf8259f1d4322959970003283cfb28b9c9d82e
TheJarki <melendez.jarki@gmail.com>
2021-05-23 20:13:58 +0200

    recipe category scraper 1.0

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import { dishScraper } from "./util/urlScraper.js";
 import { recipeScraper } from "./util/recipeScraper.js";
 
+let mainDishesUrls = [];
 
-const mainDishes = await dishScraper('https://livelytable.com/category/mains/page/1');
+const mainDishes = await dishScraper('https://livelytable.com/category/mains/page/1', mainDishesUrls);
 
 const recipe = await recipeScraper('https://livelytable.com/mini-meyer-lemon-tarts/');
 
--- util/urlScraper.js
+++ util/urlScraper.js
@@ -1,11 +1,9 @@
 import puppeteer from 'puppeteer';
 import { autoScroll } from './autoscroll.js';
 
-export var dishesUrls = [];
-
-export const dishScraper = async (url) => {
+export const dishScraper = async (url, categoryUrls) => {
     const browser = await puppeteer.launch({
-        args: ["--disable-web-security"]
+        args: ['--disable-web-security']
     });
     const page = await browser.newPage();
     await page.goto(url);
@@ -20,20 +18,20 @@ export const dishScraper = async (url) => {
         urls => urls.map(url => url.getAttribute('href')));
 
     try {
-        dishesUrls = dishesUrls.concat(urls);
+        categoryUrls.push(urls);
 
         const nextPage = await page.$eval('li.pagination-next>a', el => el.getAttribute('href'));
-
+        
         let nextPageNumber = parseInt(url.slice(-1)) + 1;
         let newUrl = url.slice(0, -1) + nextPageNumber;
-
+        
         if (nextPage) {
             await browser.close();
-            await dishScraper(newUrl);
+            await dishScraper(newUrl, categoryUrls);
         }
     } catch (e) {
         console.log('No more pages to scrap');
     }
-    return dishesUrls;
+    return [].concat(...categoryUrls);
 }
 
