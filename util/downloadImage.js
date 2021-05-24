da60a15bd47ae029d95c9d5a5502c63a1dc416bf
TheJarki <melendez.jarki@gmail.com>
2021-05-24 19:37:31 +0200

    scrap images

--- util/downloadImage.js
+++ util/downloadImage.js
@@ -1,14 +1,17 @@
 import fs from 'fs';
+import fetch from 'node-fetch';
 
 export const saveImg = async (imgPath, imgSrc) => {
 
     try {
-        const filePath = img;
+        const filePath = `./img/${imgPath}`;
         if (fs.existsSync(filePath)) return console.log(`${imgPath} already exist`);
         else {
-            fs.writeFile(filePath, data, (err) => {
+            const response = await fetch(imgSrc);
+            const buffer = await response.buffer();
+            fs.writeFile(filePath, buffer, (err) => {
                 if (err) throw err;
-                console.log(`${recipe.title} writed`);
+                console.log(`${imgPath} saved`);
             });
         }
     } catch (e) {
