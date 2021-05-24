9090a88031162097fe34cd51eda1f347e26d8af4
TheJarki <melendez.jarki@gmail.com>
2021-05-24 19:39:39 +0200

    scrap all recipes from all categories

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ let sideDishesUrls = [];
 let appetizerDishesUrls = [];
 let dessertDishesUrls = [];
 
-// await dishScraper('https://livelytable.com/category/mains/page/1', mainDishesUrls);
-// await dishScraper('https://livelytable.com/category/sides/page/1', sideDishesUrls);
-// await dishScraper('https://livelytable.com/category/appetizers/page/1', appetizerDishesUrls);
-// await dishScraper('https://livelytable.com/category/desserts/page/1', dessertDishesUrls);
+await dishScraper('https://livelytable.com/category/mains/page/1', mainDishesUrls);
+await dishScraper('https://livelytable.com/category/sides/page/1', sideDishesUrls);
+await dishScraper('https://livelytable.com/category/appetizers/page/1', appetizerDishesUrls);
+await dishScraper('https://livelytable.com/category/desserts/page/1', dessertDishesUrls);
 
 let dishesUrls = [...mainDishesUrls, ...sideDishesUrls, ...appetizerDishesUrls, ...dessertDishesUrls];
 
-recipeScraper('https://livelytable.com/shrimp-orange-and-avocado-quinoa-salad/');
-
-// for (let url of dishesUrls) {
-//     await recipeScraper(url);
-// }
+for (let url of dishesUrls) {
+    await recipeScraper(url);
+}
